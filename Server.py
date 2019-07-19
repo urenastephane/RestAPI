d92fd05dcc037c771bd8118d51af1ed761b6d32a
urenastephane <urenastephane@gmail.com>
2019-07-19 11:19:16 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -25,15 +25,19 @@ app=Flask(__name__)
 
 @app.route('/api',methods=['POST'])
 def make_predict():
-	data=request.get_json(force=True)
+	data=request.get_json(force=True) # read json from livePredict file (could be done through an interface such as Postman or Soap UI for instance)
 	test=Row(**data)
-	data=sqlContext.createDataFrame([test])
+	data=sqlContext.createDataFrame([test]) #transform data in a df to be read 
+	#add features engineered for our model
 	df=data\
 	.withColumn("timestamp", unix_timestamp(col("date")))\
 	.withColumn("time", from_unixtime(col("timestamp"), "H")*3600 + from_unixtime(col("timestamp"), "m")*60 + from_unixtime(col("timestamp"), "s"))\
+	#insert the data into the Pipeline loaded
 	transformedTestData=rfTransformerPipeline.transform(DF)
+	#Put the data formated into the model to get the prediction
 	y_hat=rfModel.transform(transformedTestData)
+	#return the prediction from that instance
 	return str(y_hat.select("prediction").head()[0])
-
+#init server
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=50111,debug=True)
